0d6da7e847c36835c3e56be33b19a827cfe8750d
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-12-11 16:43:53 -0500

    Added some window code and adjusted the code for versatility of different homework versions

--- Physics Homework Brute Force.py
+++ Physics Homework Brute Force.py
@@ -5,19 +5,51 @@ Created on Mon Nov  7 18:25:28 2022
 @author: Matthew
 """
 
-import selenium
+#import selenium 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+import tkinter as tk
+from tkinter import simpledialog
+
+
+
+
+
+urlquestion = tk.Tk()
+urlquestion.geometry("200x100")
+theurl = simpledialog.askstring("Input","Please go into the quiz and copy paste the URL in here :)",parent=urlquestion)
+urlquestion.destroy()
+
+#the url https://moodle.gatewayk12.org/2223/mod/quiz/attempt.php?attempt=5315&cmid=5948
+
+
+usernamequestion = tk.Tk()
+usernamequestion.geometry("200x100")
+theusername = simpledialog.askstring("Input","Username Please :)",parent=usernamequestion)
+usernamequestion.destroy()
+
+
+
+
+
+passwordquestion = tk.Tk()
+passwordquestion.geometry("200x100")
+thepassword = simpledialog.askstring("Input","Password Please :)",parent=passwordquestion)
+passwordquestion.destroy()
+
+
 driver = webdriver.Chrome()
-#driver.get("https://moodle.gatewayk12.org/2223/mod/quiz/attempt.php?attempt=4643&page=7&cmid=5885")#CHANGE LINK
-driver.get("https://moodle.gatewayk12.org/2223/mod/quiz/attempt.php?attempt=4643&cmid=5885#question-5039-1")
+driver.get(theurl)
+
+
 username = driver.find_element_by_name("username")
-username.send_keys("mxb100")
+username.send_keys(theusername)
 password = driver.find_element_by_name("password")
-password.send_keys("405055")
+password.send_keys(thepassword)
 signInButton = driver.find_element_by_name("signin")
 signInButton.click()
 driver.implicitly_wait(3)
@@ -27,45 +59,15 @@ yes.send_keys(Keys.ENTER)
 
 
 
-
-#maybe this registers the changes and I just need to delete the other files?
-
 handles = driver.window_handles
 driver.switch_to.window(handles[1])
 
-print(driver.title)
+ProblemNum = 1
 
+questionAttemptTotal = driver.find_element_by_xpath('/html/body/div[3]/div[5]/div/div[2]/div/section/div[2]/form/div/div[1]')
+questionAttemptPart = questionAttemptTotal.get_attribute("id")[9:13]
 
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-NextButton = driver.find_element_by_id("mod_quiz-next-nav")
-NextButton.click()
-
-
-ProblemNum = 14
-
+    
 while ProblemNum <= 20:
     isCorrect = driver.find_element_by_class_name("state")
     currentNum = 10
@@ -76,10 +78,14 @@ while ProblemNum <= 20:
     decimalsCoveredFlag = False
 
 
-    answerboxID = "q5039:"+str(ProblemNum)+"_answer"
-    checkButtonID = "q5039:"+str(ProblemNum)+"_-submit"
+    answerboxID = "q"+questionAttemptPart+":"+str(ProblemNum)+"_answer"
+    checkButtonID = "q"+questionAttemptPart+":"+str(ProblemNum)+"_-submit"
+    
+    while (str(isCorrect.text) == "Incorrect" or str(isCorrect.text) == "Not complete"):
+        
 
-    while (str(isCorrect.text) == "Incorrect"):
+        
+        
         answerbox = driver.find_element_by_id(answerboxID)
         answerbox.clear()
         answerbox.send_keys(str(currentNum))
